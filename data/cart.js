a7d11e9d82ce89b66ce82d9e0451397a9b3e024b
oussamaadda <oussamaaddaw25@gmail.com>
2024-02-10 01:33:14 +0100

    second commit

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart=[];
+export function addToCart(productId){
+    let matchigItem;
+  
+    cart.forEach((item)=>{
+        if(productId === item.productId){
+            matchigItem = item
+        }
+    })
+    
+    if(matchigItem){
+        matchigItem.quantity+=1.;
+    }else{
+        cart.push(
+            {
+                productId:productId,
+                quantity:1
+            }
+        );
+       
+    }
+  };
